b3831fd677eb7adf356627e741d83015ffc2da2b
w-vasquez <w-vasquez@DESKTOP-BE9E976>
2018-12-14 00:52:05 -0600

    Api Rest

---
+++ Web/ApiRest/Global.asax.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using System.Web.Http;
+
+namespace ApiRest
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {  
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+    }
+}
