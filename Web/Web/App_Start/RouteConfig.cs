7b9540a90f1e8b53106428e193b3ea86649aa764
w-vasquez <w-vasquez@DESKTOP-BE9E976>
2018-12-14 00:47:21 -0600

    face inicial

--- Web/Web/App_Start/RouteConfig.cs
+++ Web/Web/App_Start/RouteConfig.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 
@@ -9,7 +7,7 @@ namespace Web
 {
     public class RouteConfig
     {
-        public static void RegisterRoutes(RouteCollection routes) 
+        public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
